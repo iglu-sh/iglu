d945710c0b7778364bf192cfe20e9f30b2dfee1f
Svenum <s.ziegler@holypenguin.net>
2026-04-17 12:45:51 +0200

    feat(builder): implement parallel websockets and session restore

--- builder/app/builder.py
+++ builder/app/builder.py
@@ -1,21 +1,42 @@
 import shutil
 import asyncio
-import os
-from fastapi import WebSocket
-from pty import openpty
 import re
-from app.types import Config
-import app.msg as msg
+import os
+from datetime import datetime
+from asyncio.subprocess import Process
+from pty import openpty
+from pydantic import TypeAdapter, ValidationError
+from typing import Any
+from app.connection_manager import ConnectionManager
+from app.types import Config, IgluResponse
+from app.types import PreDefinedResponse
 
 class Builder:
     """This class provides all functinality to build and push derivations"""
-
     _config: Config | None = None
+    _master_fd: int | None = None
+    _process: Process | None = None
 
     @classmethod
-    def set_config(cls, new_config: Config) -> None:
+    def set_config(cls, new_config: Any) -> bool: # pyright: ignore[reportExplicitAny, reportAny]
         """Set the config of the builder"""
-        cls._config = new_config
+        try:
+            cls._config = TypeAdapter(Config).validate_python(new_config)
+            return True
+        except(ValidationError):
+            return False
+
+    @classmethod
+    def process_is_running(cls) -> bool:
+        """Check if a process is already running
+        
+        Returns.
+            bool: Is a process currently running?
+        """
+        if cls._process is None:
+            return False
+        else:
+            return True
 
     @staticmethod
     def check_health() -> bool:
@@ -27,21 +48,13 @@ class Builder:
         return shutil.which("cachix") is not None
 
     @classmethod
-    async def build(cls, websocket: WebSocket) -> None:
-        """Start the build of a derivation
-        Parameters:
-            websocket (WebSocket): The websocket which is used to show the stdout and stderr
-        """
-        if cls._config is None:
-            await websocket.send_text("No config set!")
-        else:
-            await websocket.send_text("Build is starting...")
-
-            # Create pty
-            master_fd, slave_fd = openpty()
+    async def create_process(cls) -> None:
+        """This Method creates a new process if needed"""
+        if cls._process is None and cls._master_fd is None and not cls._config is None:
+            cls._master_fd, slave_fd = openpty()
 
             # Start process on pty
-            process = await asyncio.create_subprocess_exec(
+            cls._process = await asyncio.create_subprocess_exec(
                 cls._config["command"][0], *cls._config["command"][1:],
                 stdout=slave_fd, 
                 stderr=slave_fd,
@@ -51,26 +64,75 @@ class Builder:
             # Close slave_fd as it is note needed anymore
             os.close(slave_fd)
 
-            while process.returncode is None:
-                try:
-                    data = await asyncio.get_event_loop().run_in_executor(
-                        None, os.read, master_fd, 1024
-                    )
-                except OSError:
-                    break
+    @classmethod
+    async def build(cls) -> None:
+        """Start the build of a derivation
+        Parameters:
+            websocket (WebSocket): The websocket which is used to show the stdout and stderr
+        """
 
-                # Remove ASCI control sequences
-                ansi_escape = re.compile(r'\x1b\[[0-9;]*[a-zA-Z]|\x1b\([a-zA-Z]')
-                clean = ansi_escape.sub("", data.decode()).strip()
+        # Check if config is present
+        if cls._config is None:
+            await ConnectionManager.broadcast(PreDefinedResponse.INVALID_CONFIG())
+        else:
+            await ConnectionManager.broadcast(PreDefinedResponse.STARTING_BUILD())
 
-                if clean:
-                    await websocket.send_json(msg.gen_command_output(clean))
+            # Start process
+            await cls.create_process()
 
-            if process.returncode == 0:
-                await websocket.send_json(msg.gen_build_succeeded())
-            else:
-                await websocket.send_json(msg.gen_build_failed())
+            if not cls._process is None and not cls._master_fd is None:
+                # While the process run wait for output
+                # TODO: Split in stderr and stdout
+                while cls._process.returncode is None:
+                    try:
+                        # Get each output line
+                        data = await asyncio.get_event_loop().run_in_executor(
+                            None, os.read, cls._master_fd, 1024
+                        )
+                    except OSError:
+                        break
 
-        await websocket.close()
+                    # Remove ASCI control sequences
+                    ansi_escape = re.compile(r'\x1b\[[0-9;]*[a-zA-Z]|\x1b\([a-zA-Z]')
+                    clean = ansi_escape.sub("", data.decode()).strip()
 
+                    # Send lines which contain data to the client
+                    if clean:
+                        await ConnectionManager.broadcast(cls._gen_command_output_msg(clean))
+                        clean = None
+
+                # Check exit code of process
+                if cls._process.returncode == 0:
+                    await ConnectionManager.broadcast(PreDefinedResponse.BUILD_SUCCEEDED())
+                else:
+                    await ConnectionManager.broadcast(PreDefinedResponse.BUILD_FAILED())
+
+        # close process and pty
+        if not cls._master_fd is None:
+            os.close(cls._master_fd)
+        cls._process = None
+        cls._master_fd = None
+
+        # close all websockets
+        ConnectionManager.disconnect_all()
+
+    @staticmethod
+    def _gen_command_output_msg(output: str) -> IgluResponse:
+        """Generate a command_output IgluResponse
+
+        Parameters:
+            output (str): the output of the command
+
+        Returns:
+            IgluResponse: "invalid config" response
+        """
+        res = IgluResponse({
+            "status_code": 200,
+            "status_message": "OK",
+            "data": {"msg": output},
+            "is_error": False,
+            "timestamp": datetime.now().isoformat()
+        })
+
+        return TypeAdapter(IgluResponse).validate_python(res)
 
---
+++ builder/app/connection_manager.py
@@ -0,0 +1,50 @@
+from fastapi import WebSocket
+
+from app.types import IgluResponse
+
+class ConnectionManager():
+    _active_connections: list[WebSocket] = []
+
+    @classmethod
+    async def connect(cls, websocket: WebSocket) -> None:
+        """Add a new websocket connection to the list
+        
+        Parameters:
+            websocket (WebSocket): the websocket to add
+        """
+        await websocket.accept()
+        cls._active_connections.append(websocket)
+
+    @classmethod
+    def disconnect(cls, websocket: WebSocket) -> None:
+        """Remove a single connnection from the connection list
+        
+        Parameters:
+            websocket (WebSocket): the websocket to remove 
+        """
+        cls._active_connections.remove(websocket)
+
+    @classmethod
+    def disconnect_all(cls) -> None:
+        """Clear the hole connection list"""
+        cls._active_connections.clear()
+
+    @classmethod
+    async def direct_message(cls, data: IgluResponse, websocket: WebSocket) -> None:
+        """Send a message to only one client
+
+        Parameters:
+            data (IgluResponse): the response which should be send to the client
+            websocket (WebSocket): the websocket to which the message should be send
+        """
+        await websocket.send_json(data)
+
+    @classmethod
+    async def broadcast(cls, data: IgluResponse) -> None:
+        """Sends a message to all websocket connections
+        
+        Parameters:
+            data (IgluResponse): the response which should be send to all websockets
+        """
+        for connection in cls._active_connections:
+            await connection.send_json(data)
