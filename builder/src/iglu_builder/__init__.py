f5674cc82a4c05bf78a9a0c5fab87660f8319ac4
Svenum <s.ziegler@holypenguin.net>
2026-05-06 08:22:16 +0200

    feat(builder): add cache settings to config.toml + fix some bugs

--- builder/src/iglu_builder/__init__.py
+++ builder/src/iglu_builder/__init__.py
@@ -1,4 +1,8 @@
 import os
 from pathlib import Path
 
-DEFAULT_CONF_PATH = Path(os.getcwd() + "/config.toml")
+# Load default config path if no path is given by IGLU_BUILDER_CONF
+default_path = os.getcwd() + "/config.toml"
+env_path = os.getenv("IGLU_BUILDER_CONF")
+
+CONF_PATH = Path(os.path.abspath(env_path or default_path))
