8ecdb49b24f4ddd7485672923efdc809f332e17b
Svenum <s.ziegler@holypenguin.net>
2026-05-09 21:28:51 +0200

    chore: export Logger in shared module

--- shared/index.ts
+++ shared/index.ts
@@ -1,7 +1,9 @@
 import * as dbI from "./db";
 import * as filesI from "./files";
 import * as utilsI from "./utils/";
+import * as loggerI from "./logger/Logger.ts";
 
 export import db = dbI;
 export import files = filesI;
 export import utils = utilsI;
+export import Logger = loggerI;
