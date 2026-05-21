9ef3d4ab76217cc70e3334194d654634b11a90f4
Svenum <s.ziegler@holypenguin.net>
2026-05-21 18:17:06 +0200

    fix: add missing arch in tag

--- nix/packages/iglu-cache-docker.nix
+++ nix/packages/iglu-cache-docker.nix
@@ -9,10 +9,11 @@
 
 let
   inherit (self.packages.${stdenv.hostPlatform.system}) iglu-cache;
+  archType = if (stdenv.hostPlatform.system == "x86_64-linux") then "amd64" else "arm64";
 in
 dockerTools.buildImage {
   name = iglu-cache.pname;
-  tag = "v${iglu-cache.version}";
+  tag = "v${iglu-cache.version}-${archType}";
   created = "now";
 
   copyToRoot = buildEnv {
